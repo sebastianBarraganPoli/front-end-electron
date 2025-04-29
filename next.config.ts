f3732758bc240d5562ebd8c8df86100ff3fe8a2e
Sebastian Barragan <sbarraganb@poligran.edu.co>
2025-04-29 17:43:58 -0500

    Cambios para publicar

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: 'export',
+  // Nombre de tu repositorio
+  basePath: '/electron-store',
+  images: {
+    unoptimized: true,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
